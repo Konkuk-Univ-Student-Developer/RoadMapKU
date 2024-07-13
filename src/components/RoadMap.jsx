541f225dedb15e7a3888cebe1c4cd17179bca440
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2024-07-13 22:53:23 +0900

    add share button

--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -1,7 +1,27 @@
+import styled from 'styled-components';
+
+const ShareButton = styled.a`
+	position: fixed;
+	right: 20px;
+	bottom: 20px;
+	padding: 10px 20px;
+	background-color: #007bff;
+	color: white;
+	text-align: center;
+	border-radius: 5px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+	transition: background-color 0.3s;
+
+	&:hover {
+		background-color: #0056b3;
+	}
+`;
+
 function RoadMap() {
 	return (
 		<>
-			<h1>road map page!</h1>
+			<h1>Road Map Page!</h1>
+			<ShareButton>Share</ShareButton>
 		</>
 	);
 }
