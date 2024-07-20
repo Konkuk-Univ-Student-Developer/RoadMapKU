5fe507ad9f58d2c21ac9a7be62c69462ac487e67
HA <haandy98@konkuk.ac.kr>
2024-07-20 19:37:18 +0900

    item add, delete 기능 추가

--- src/Animation/Animation.jsx
+++ src/Animation/Animation.jsx
@@ -30,6 +30,24 @@ export const dismissBounce = keyframes`
   }
 `;
 
+export const slideUp = keyframes`
+  from {
+    transform: translateY(100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+`;
+
+export const slideDown = keyframes`
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(100%);
+  }
+`;
+
 export const immergeFade = keyframes`
   0% {
     opacity: 0;
