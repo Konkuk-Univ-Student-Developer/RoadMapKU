339961d1ffde858aeba9e04b4e08c3671817813f
Rune <sungjong5k@naver.com>
2024-07-23 00:01:56 +0900

    주석 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import RoadMap from './pages/RoadMap';
-// import { Sidebar } from './components/SideBar';
-// import { MainContent } from './components/MainContent';
 
 function App() {
 	return (
