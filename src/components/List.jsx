c632df1f9718e68960e11638d288844e439b4621
HA <haandy98@konkuk.ac.kr>
2024-07-23 13:07:01 +0900

    코스 변경 기능 추가

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -23,7 +23,6 @@ const Content = styled.div`
 const Title = styled.div`
 	margin-right: auto;
 	margin-left: 1rem;
-	margin-top: 1rem;
 	font-size: xx-large;
 	font-weight: bolder;
 `;
@@ -48,6 +47,7 @@ const ListItem = styled.li`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	text-align: center;
 	grid-column: ${({ column }) => column};
 	grid-row: ${({ row }) => row};
 	cursor: ${({ unclickable }) => (unclickable ? 'default' : 'pointer')};
@@ -98,98 +98,19 @@ const indexItems = [
 	{ content: ['3-1'], row: 1, column: 6 },
 	{ content: ['3-2'], row: 1, column: 7 },
 	{ content: ['4-1'], row: 1, column: 8 },
-	{ content: ['4-2'], row: 1, column: 9 },
-	{ content: ['1'], row: 2, column: 1 },
-	{ content: ['2'], row: 3, column: 1 },
-	{ content: ['3'], row: 4, column: 1 },
-	{ content: ['4'], row: 5, column: 1 },
-	{ content: ['5'], row: 6, column: 1 },
-	{ content: ['6'], row: 7, column: 1 },
-	{ content: ['7'], row: 8, column: 1 },
-	{ content: ['8'], row: 9, column: 1 },
-	{ content: ['9'], row: 10, column: 1 },
-	{ content: ['10'], row: 11, column: 1 },
-	{ content: ['11'], row: 12, column: 1 },
-	{ content: ['12'], row: 13, column: 1 },
-	{ content: ['13'], row: 14, column: 1 },
-	{ content: ['14'], row: 15, column: 1 }
+	{ content: ['4-2'], row: 1, column: 9 }
 ];
 
-const initialItems = [
-	{ content: ['C프로그래밍'], row: 3, column: 2, code: 100, newRow: 0 },
-	{ content: ['컴퓨터공학개론'], row: 11, column: 2, code: 102, newRow: 0 },
-	{ content: ['테크니컬영어발표'], row: 12, column: 2, code: 103, newRow: 0 },
-
-	{ content: ['JAVA프로그래밍'], row: 3, column: 3, code: 104, newRow: 0 },
-	{ content: ['테크니컬영어말하기'], row: 12, column: 3, code: 105, newRow: 0 },
-
-	{ content: ['웹프로그래밍'], row: 2, column: 4, code: 201, newRow: 0 },
-	{ content: ['객체지향프로그래밍'], row: 3, column: 4, code: 202, newRow: 0 },
-	{ content: ['자료구조'], row: 6, column: 4, code: 203, newRow: 0 },
-	{ content: ['테크니컬영문독해'], row: 12, column: 4, code: 204, newRow: 0 },
-	{ content: ['전공기초프로젝트1'], row: 13, column: 4, code: 205, newRow: 0 },
-	{ content: ['오픈소스SW입문'], row: 14, column: 4, code: 206, newRow: 0 },
-
-	{ content: ['소프트웨어공학'], row: 3, column: 5, code: 207, newRow: 0 },
-	{ content: ['컴퓨테이션이론'], row: 4, column: 5, code: 208, newRow: 0 },
-	{ content: ['시스템프로그래밍'], row: 5, column: 5, code: 209, newRow: 0 },
-	{ content: ['알고리즘'], row: 6, column: 5, code: 210, newRow: 0 },
-	{ content: ['컴퓨터회로'], row: 7, column: 5, code: 211, newRow: 0 },
-	{ content: ['인공지능'], row: 10, column: 5, code: 212, newRow: 0 },
-	{ content: ['데이터통신'], row: 11, column: 5, code: 213, newRow: 0 },
-	{ content: ['랜덤프로세스개론'], row: 12, column: 5, code: 214, newRow: 0 },
-	{ content: ['전공기초프로젝트2'], row: 13, column: 5, code: 215, newRow: 0 },
-
-	{ content: ['모바일프로그래밍'], row: 2, column: 6, code: 301, newRow: 0 },
-	{ content: ['객체지향개발방법론'], row: 3, column: 6, code: 302, newRow: 0 },
-	{ content: ['프로그래밍언어론'], row: 4, column: 6, code: 303, newRow: 0 },
-	{ content: ['운영체제'], row: 5, column: 6, code: 304, newRow: 0 },
-	{ content: ['알고리즘연습'], row: 6, column: 6, code: 305, newRow: 0 },
-	{ content: ['컴퓨터구조'], row: 7, column: 6, code: 306, newRow: 0 },
-	{ content: ['컴퓨터그래픽스'], row: 8, column: 6, code: 307, newRow: 0 },
-	{ content: ['데이터베이스'], row: 9, column: 6, code: 308, newRow: 0 },
-	{ content: ['디지털영상처리'], row: 10, column: 6, code: 309, newRow: 0 },
-	{ content: ['컴퓨터네트워크'], row: 11, column: 6, code: 310, newRow: 0 },
-	{ content: ['시그널프로세싱'], row: 12, column: 6, code: 311, newRow: 0 },
-	{ content: ['K-Lab프로젝트1'], row: 13, column: 6, code: 312, newRow: 0 },
-	{ content: ['산학협력프로젝트1'], row: 14, column: 6, code: 313, newRow: 0 },
-	{ content: ['오픈소스SW프로젝트1'], row: 15, column: 6, code: 314, newRow: 0 },
-
-	{ content: ['클라우드웹서비스'], row: 2, column: 7, code: 315, newRow: 0 },
-	{ content: ['소프트웨어아키텍쳐'], row: 3, column: 7, code: 316, newRow: 0 },
-	{ content: ['컴파일러'], row: 4, column: 7, code: 317, newRow: 0 },
-	{ content: ['클라우드컴퓨팅'], row: 5, column: 7, code: 318, newRow: 0 },
-	{ content: ['암호학'], row: 6, column: 7, code: 319, newRow: 0 },
-	{ content: ['병렬프로그래밍'], row: 7, column: 7, code: 320, newRow: 0 },
-	{ content: ['고급컴퓨터그래픽스'], row: 8, column: 7, code: 321, newRow: 0 },
-	{ content: ['데이터사이언스'], row: 9, column: 7, code: 322, newRow: 0 },
-	{ content: ['기계학습'], row: 10, column: 7, code: 323, newRow: 0 },
-	{ content: ['네트워크프로그래밍'], row: 11, column: 7, code: 324, newRow: 0 },
-	{ content: ['멀티미디어공학'], row: 12, column: 7, code: 325, newRow: 0 },
-	{ content: ['K-Lab프로젝트2'], row: 13, column: 7, code: 326, newRow: 0 },
-	{ content: ['산학협력프로젝트2'], row: 14, column: 7, code: 327, newRow: 0 },
-	{ content: ['오픈소스SW프로젝트2'], row: 15, column: 7, code: 328, newRow: 0 },
-
-	{ content: ['클라우드IoT서비스'], row: 2, column: 8, code: 401, newRow: 0 },
-	{ content: ['소프트웨어V&V'], row: 3, column: 8, code: 402, newRow: 0 },
-	{ content: ['임베디드시스템소프트웨어'], row: 4, column: 8, code: 403, newRow: 0 },
-	{ content: ['분산시스템및컴퓨팅'], row: 5, column: 8, code: 404, newRow: 0 },
-	{ content: ['협동분산시스템'], row: 6, column: 8, code: 405, newRow: 0 },
-	{ content: ['가상현실'], row: 7, column: 8, code: 406, newRow: 0 },
-	{ content: ['HCI'], row: 8, column: 8, code: 407, newRow: 0 },
-	{ content: ['컴퓨터비전'], row: 9, column: 8, code: 408, newRow: 0 },
-	{ content: ['자연어처리'], row: 10, column: 8, code: 409, newRow: 0 },
-	{ content: ['테크니컬영어글쓰기'], row: 12, column: 8, code: 410, newRow: 0 },
-	{ content: ['졸업프로젝트1'], row: 13, column: 8, code: 411, newRow: 0 },
-
-	{ content: ['웹기술및응용'], row: 2, column: 9, code: 412, newRow: 0 },
-	{ content: ['차세대분산시스템'], row: 5, column: 9, code: 413, newRow: 0 },
-	{ content: ['컴퓨터정보보안'], row: 6, column: 9, code: 414, newRow: 0 },
-	{ content: ['게임프로그래밍'], row: 7, column: 9, code: 415, newRow: 0 },
-	{ content: ['UX디자인'], row: 8, column: 9, code: 416, newRow: 0 },
-	{ content: ['무선및이동네트워크'], row: 11, column: 9, code: 417, newRow: 0 },
-	{ content: ['컴퓨터공학세미나'], row: 12, column: 9, code: 418, newRow: 0 },
-	{ content: ['졸업프로젝트2'], row: 13, column: 9, code: 419, newRow: 0 }
+const indexItems_my = [
+	{ content: [''], row: 1, column: 1 },
+	{ content: ['1-1'], row: 1, column: 2 },
+	{ content: ['1-2'], row: 1, column: 3 },
+	{ content: ['2-1'], row: 1, column: 4 },
+	{ content: ['2-2'], row: 1, column: 5 },
+	{ content: ['3-1'], row: 1, column: 6 },
+	{ content: ['3-2'], row: 1, column: 7 },
+	{ content: ['4-1'], row: 1, column: 8 },
+	{ content: ['4-2'], row: 1, column: 9 }
 ];
 
 const lastIndexList = [
@@ -203,13 +124,25 @@ const lastIndexList = [
 	{ semester: '4-2', index: 1, column: 9 }
 ];
 
-const List = ({ show }) => {
-	const [roadMapItems, setRoadMapItems] = useState(indexItems);
-	const [myItems, setMyItems] = useState(indexItems);
+const List = ({ show, courseItems }) => {
+	const [roadMapItems, setRoadMapItems] = useState(indexItems_my);
+	const [myItems, setMyItems] = useState(indexItems_my);
 	const [lastIndex, setLastIndex] = useState(lastIndexList);
+	const [maxRowValue, setMaxRowValue] = useState(0);
 
 	useEffect(() => {
-		initialItems.forEach((item, index) => {
+		const newItems = [...indexItems];
+		setRoadMapItems(newItems);
+
+		const rowValues = courseItems.map((item) => item.row);
+		const maxRowValueFromCourseItems = Math.max(...rowValues);
+		for (let i = 1; i < maxRowValueFromCourseItems; i++) {
+			newItems.push({ content: [`${i}`], row: i + 1, column: 1 });
+		}
+		setRoadMapItems([...newItems]);
+		setMaxRowValue(maxRowValueFromCourseItems);
+
+		courseItems.forEach((item, index) => {
 			setTimeout(() => {
 				setRoadMapItems((prevItems) => {
 					if (!prevItems.some((prevItem) => prevItem.content === item.content)) {
@@ -219,7 +152,7 @@ const List = ({ show }) => {
 				});
 			}, index * 10);
 		});
-	}, []);
+	}, [courseItems]);
 
 	const addItemToMyList = (selectedItem) => {
 		const updatedLastIndexList = lastIndex.map((item) => {
@@ -292,6 +225,24 @@ const List = ({ show }) => {
 		setSelectedItem(null);
 	}
 
+	const [buttonDisabled, setButtonDisabled] = useState(false);
+	const handleItemClickList = (item) => {
+		if (item.unclickable || buttonDisabled) return;
+		setButtonDisabled(true);
+		removeItemFromList(item);
+		setTimeout(() => {
+			setButtonDisabled(false);
+		}, 500);
+	};
+	const handleItemClickMyList = (item) => {
+		if (item.unclickable || buttonDisabled) return;
+		setButtonDisabled(true);
+		removeItemFromMyList(item);
+		setTimeout(() => {
+			setButtonDisabled(false);
+		}, 500);
+	};
+
 	const listItems = roadMapItems.map((item) => {
 		const isUnclickable = item.row === 1 || item.column === 1;
 		const isIndexItems = item.column === 1;
@@ -312,7 +263,7 @@ const List = ({ show }) => {
             ${isIndexItems ? 'small-size' : ''}
             `}
 					// onClick={!isUnclickable ? () => showModal(index) : undefined}
-					onClick={!isUnclickable ? () => removeItemFromList(item) : undefined}
+					onClick={() => handleItemClickList(item)}
 					style={{
 						gridColumn: item.column,
 						gridRow: item.row,
@@ -346,7 +297,7 @@ const List = ({ show }) => {
             ${isIndexItems ? 'small-size' : ''}
             `}
 					// onClick={!isUnclickable ? () => showModal(item) : undefined}
-					onClick={!isUnclickable ? () => removeItemFromMyList(item) : undefined}
+					onClick={() => handleItemClickMyList(item)}
 					style={{
 						gridColumn: item.column,
 						gridRow: item.newRow,
@@ -358,6 +309,8 @@ const List = ({ show }) => {
 			</CSSTransition>
 		);
 	});
+
+	// console.log('MaxRow: ', maxRowValue);
 
 	return (
 		<Content className={`content ${show ? 'with-sidebar' : 'full-width'}`}>
