1ae640923f08dcfd557b15cce0004249580d39da
Rune <sungjong5k@naver.com>
2024-07-31 01:26:03 +0900

    소분류 fetch 함수 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,16 @@ import Header from '../components/Header';
 import useClient from '../hooks/useClient';
 import { useEffect } from 'react';
 function Home() {
-	const { fetchLargeField, fetchMiddleField } = useClient();
+	const { fetchLargeField, fetchMiddleField, fetchSmallField } = useClient();
 	useEffect(() => {
 		fetchLargeField();
 		fetchMiddleField({
 			largeFieldCode: '01000000',
 			largeField: '경영·사무·금융·보험직'
+		});
+		fetchSmallField({
+			middleFieldCode: '01010000',
+			middleField: '관리직(임원,부서장)'
 		});
 	});
 
