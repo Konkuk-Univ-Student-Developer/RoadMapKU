dc91fc45b1ded121fd9473e5ba68c4c9fbb3769d
Rune <sungjong5k@naver.com>
2024-07-31 02:00:10 +0900

    atom 생성 및 RecoilRoot 추가

---
+++ src/recoils/atoms.js
@@ -0,0 +1,21 @@
+import { atom } from 'recoil';
+
+export const largeFieldState = atom({
+	key: 'largeFieldState',
+	default: []
+});
+
+export const middleFieldState = atom({
+	key: 'middleFieldState',
+	default: []
+});
+
+export const smallFieldState = atom({
+	key: 'smallFieldState',
+	default: []
+});
+
+export const detailFieldState = atom({
+	key: 'detailFieldState',
+	default: []
+});
