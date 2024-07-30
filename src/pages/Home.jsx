f4bd5d3ecc27acf476758aaf7905cdd6392f24e0
Rune <sungjong5k@naver.com>
2024-07-31 01:33:04 +0900

    세분류 fetch 함수 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import useClient from '../hooks/useClient';
 import { useEffect } from 'react';
 function Home() {
-	const { fetchLargeField, fetchMiddleField, fetchSmallField } = useClient();
+	const { fetchLargeField, fetchMiddleField, fetchSmallField, fetchDetailField } = useClient();
 	useEffect(() => {
 		fetchLargeField();
 		fetchMiddleField({
@@ -13,6 +13,10 @@ function Home() {
 		fetchSmallField({
 			middleFieldCode: '01010000',
 			middleField: '관리직(임원,부서장)'
+		});
+		fetchDetailField({
+			smallFieldCode: '01010100',
+			smallField: '의회의원·고위공무원 및 기업 고위임원'
 		});
 	});
 
