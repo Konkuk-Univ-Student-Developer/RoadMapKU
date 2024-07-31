423742adb87ef77a42f0643ae94923ad1760b508
Rune <sungjong5k@naver.com>
2024-07-31 21:52:43 +0900

    선택한 분야를 recoil을 통해 출력

--- src/components/FieldCategory.jsx
+++ src/components/FieldCategory.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+import { selectedFieldState } from '../recoils/atoms';
 
 const FieldCategoryContainer = styled.div`
 	display: flex;
@@ -42,6 +44,7 @@ export const FixButton = styled.button`
 `;
 
 const FieldCategory = () => {
+	const fieldState = useRecoilValue(selectedFieldState);
 	const onClickHandler = () => {
 		alert('진출분야를 바꿀 수 있습니다!');
 	};
@@ -50,19 +53,19 @@ const FieldCategory = () => {
 			<FieldCategoryContainer>
 				<ItemContainer>
 					<Title>대분류</Title>
-					<Description>연구직 및 공학 기술직</Description>
+					<Description>{fieldState.largeField}</Description>
 				</ItemContainer>
 				<ItemContainer>
 					<Title>중분류</Title>
-					<Description>정보통신 연구개발직 및 공학기술직</Description>
+					<Description>{fieldState.middleField}</Description>
 				</ItemContainer>
 				<ItemContainer>
 					<Title>소분류</Title>
-					<Description>소프트웨어 개발자</Description>
+					<Description>{fieldState.smallField}</Description>
 				</ItemContainer>
 				<ItemContainer>
 					<Title>세분류</Title>
-					<Description>웹 개발자</Description>
+					<Description>{fieldState.detailField}</Description>
 				</ItemContainer>
 				<ItemContainer>
 					<FixButton onClick={onClickHandler}>설정 바꾸기</FixButton>
