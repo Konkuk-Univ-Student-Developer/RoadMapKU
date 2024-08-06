987f56e592fdad8f42b241e8cf7d0bfc9a8129a1
YeeunKim <katekim512@konkuk.ac.kr>
2024-08-06 14:54:27 +0900

    [Feat 💡] Modal 생성 및 정리 + X표

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,206 +1,42 @@
-// import React, { useState, useEffect } from 'react';
-// import styled from 'styled-components';
-// import { CSSTransition } from 'react-transition-group';
-// import { immergeBounce, dismissBounce } from '../Animation/Animation';
+import React, { useEffect, useRef } from 'react';
+import useOutSideClick from '../../hooks/useOutsideClick';
+import ModalContainer from './ModalContainer';
+import * as L from './ModalStyles';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// const animationTiming = {
-// 	enter: 1000,
-// 	exit: 1000
-// };
-
-// // Styled components
-// const ModalWrapper = styled.div`
-// 	position: fixed;
-// 	z-index: 200;
-// 	border: 0.05rem solid black;
-// 	background-color: white;
-// 	padding: 10px;
-// 	text-align: center;
-// 	box-sizing: border-box;
-// 	top: 25%;
-// 	left: 25%;
-// 	width: 50%;
-
-// 	&.BounceImmerge {
-// 		animation: ${immergeBounce} 400ms ease-out forwards;
-// 	}
-
-// 	&.BounceDismiss {
-// 		animation: ${dismissBounce} 400ms ease-out forwards;
-// 	}
-// `;
-
-// const ModalContent = styled.div`
-// 	font-family: Arial;
-// 	font-size: 1rem;
-// 	text-align: start;
-// 	margin: 10px 0;
-// `;
-
-// const Modal = (props) => {
-// const [modalContent, setModalContent] = useState(null);
-// const [modalContent_sub1, setModalContent_sub1] = useState(null);
-// const [modalContent_sub2, setModalContent_sub2] = useState(null);
-// const [modalContent_sub3, setModalContent_sub3] = useState(null);
-
-// 	useEffect(() => {
-// 		if (props.show) {
-// 			setModalContent(props.item.content[0]);
-// 			setModalContent_sub1(props.item.content[0]);
-// 			setModalContent_sub2(props.item.content[0]);
-// 			setModalContent_sub3(props.item.content[0]);
-// 		}
-// 	}, [props.show, props.content]);
-
-// 	return (
-// 		<CSSTransition
-// 			in={props.show}
-// 			timeout={animationTiming}
-// 			mountOnEnter
-// 			unmountOnExit
-// 			classNames={{
-// 				appear: 'BounceImmerge',
-// 				enter: 'BounceImmerge',
-// 				exit: 'BounceDismiss'
-// 			}}
-// 		>
-// 			<ModalWrapper>
-// 				<h1>{modalContent}</h1>
-// 				<h2>
-// 					<ModalContent>내용1: {modalContent_sub1}</ModalContent>
-// 				</h2>
-// 				<h3>
-// 					<ModalContent>내용2: {modalContent_sub2}</ModalContent>
-// 				</h3>
-// 				<h4>
-// 					<ModalContent>내용3: {modalContent_sub3}</ModalContent>
-// 				</h4>
-// 				<button onClick={props.closed}>닫기</button>
-// 			</ModalWrapper>
-// 		</CSSTransition>
-// 	);
-// };
-
-// export default Modal;
-
-//------------------------------------------------------------
-import React, { useState, useEffect } from 'react';
-import { CSSTransition } from 'react-transition-group';
-import { useRecoilValue } from 'recoil';
-import { couseDetailState } from '../../recoils/atoms';
-import useClient from '../../hooks/useClient';
-import {
-	Overlay,
-	ModalWrapper,
-	Title,
-	Subtitle,
-	ModalContent,
-	ButtonContainer,
-	Button,
-	Subject,
-	SubjectContainer,
-	TableContent
-} from './ModalStyles';
-import TableComponent from '../TableComponent';
-import TableComponent2 from '../TableComponent2';
-
-const Modal = ({ show, width, closed }) => {
-	const [scrollPosition, setScrollPosition] = useState(0);
-	const couseDetail = useRecoilValue(couseDetailState);
-	const { fetchCourseDetail } = useClient();
-
-	useEffect(() => {
-		if (show) {
-			fetchCourseDetail();
-		}
-	}, [show, fetchCourseDetail]);
-
-	useEffect(() => {
-		if (show) {
-			setScrollPosition(window.scrollY);
-			document.body.style.overflow = 'hidden';
-		} else {
-			document.body.style.overflow = 'auto';
-			window.scrollTo(0, scrollPosition);
-		}
-	}, [show, scrollPosition]);
-
-	const handleOverlayClick = (e) => {
-		if (e.target === e.currentTarget) {
-			closed();
-		}
+function Modal({ onClose, children }) {
+	const modalRef = useRef(null);
+	const handleClose = () => {
+		onClose?.();
 	};
 
-	if (!couseDetail.length) return null;
+	useOutSideClick(modalRef, handleClose);
+	useEffect(() => {
+		const $body = document.querySelector('body');
+		$body.style.overflow = 'hidden';
+		return () => ($body.style.overflow = 'auto');
+	}, []);
 
-	const modalContent = couseDetail[3]; // 예시로 두 번째 데이터를 사용
-
-	const additionalInfo = modalContent.Addimfomation;
-
-	const tableData = [
-		['개설대학', additionalInfo.openingCollegeName],
-		['개설학과', additionalInfo.openingSubjectName],
-		['강의유형', additionalInfo.lectureType],
-		['학년', additionalInfo.openingSchoolYear.toString()],
-		['학기', additionalInfo.openingSemesterTerm],
-		['학점', additionalInfo.time.toString()],
-		['공학인증구분', additionalInfo.engineeringCertificationFlagCode === 0 ? 'N' : 'Y'],
-		['선수강과목', additionalInfo.preCourse],
-		['MOOC 여부', additionalInfo.moocFlag === 0 ? 'N' : 'Y'],
-		['Selc 여부', additionalInfo.selcFlag === 0 ? 'N' : 'Y'],
-		['챌린저여부', additionalInfo.dreamSemesterFlag === 0 ? 'N' : 'Y']
-	];
-
-	const tableData2 = [
-		[modalContent.competency.competencyName1, modalContent.competency.competencyRemark1],
-		[modalContent.competency.competencyName2, modalContent.competency.competencyRemark2],
-		[modalContent.competency.competencyName3, modalContent.competency.competencyRemark3]
-	];
-
-	//const courseTableData = modalContent.Addimfomation.table ? modalContent.Addimfomation.table.rows : [];
-
+	useEffect(() => {
+		const $body = document.querySelector('body');
+		const overflow = $body.style.overflow;
+		$body.style.overflow = 'hidden';
+		return () => {
+			$body.style.overflow = overflow;
+		};
+	}, []);
 	return (
-		<CSSTransition
-			in={show}
-			mountOnEnter
-			unmountOnExit
-			classNames={{
-				appear: 'BounceImmerge',
-				enter: 'BounceImmerge',
-				exit: 'BounceDismiss'
-			}}
-		>
-			<Overlay onClick={handleOverlayClick}>
-				<ModalWrapper width={width} onClick={(e) => e.stopPropagation()}>
-					<Title>Course Infomation</Title>
-					<SubjectContainer>
-						<Subject>
-							{modalContent.typicalKoreanName} ({modalContent.typicalEnglishName})
-						</Subject>
-					</SubjectContainer>
-					<Subtitle>국문설명</Subtitle>
-					<ModalContent>{modalContent.koreanDescription}</ModalContent>
-					<Subtitle>영문설명</Subtitle>
-					<ModalContent>{modalContent.englishDescription}</ModalContent>
-					{/* <Subtitle>교과 세부 정보</Subtitle>
-					<ModalContent>
-						<TableComponent data={courseTableData} />
-					</ModalContent> */}
-					<Subtitle>Additional Information</Subtitle>
-					<TableContent>
-						<TableComponent data={tableData} />
-					</TableContent>
-					<Subtitle>전공 역량</Subtitle>
-					<TableContent>
-						<TableComponent2 data={tableData2} />
-					</TableContent>
-					<ButtonContainer>
-						<Button onClick={closed}>Close</Button>
-					</ButtonContainer>
-				</ModalWrapper>
-			</Overlay>
-		</CSSTransition>
+		<ModalContainer>
+			<L.Overlay>
+				<L.ModalWrap ref={modalRef}>
+					<L.ButtonContainer>
+						<i className="fa-solid fa-xmark" onClick={handleClose} style={{ cursor: 'pointer', fontSize: '30px' }}></i>
+					</L.ButtonContainer>
+					<L.Contents>{children}</L.Contents>
+				</L.ModalWrap>
+			</L.Overlay>
+		</ModalContainer>
 	);
-};
+}
 
 export default Modal;
--- src/components/Modal/ModalStyles.jsx
+++ src/components/Modal/ModalStyles.jsx
@@ -1,45 +1,53 @@
 import styled from 'styled-components';
 import { immergeBounce, dismissBounce } from '../../Animation/Animation';
 
-export const Overlay = styled.div`
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	background: rgba(0, 0, 0, 0.5); /* 반투명 배경 */
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	z-index: 1000;
-`;
+// export const Overlay = styled.div`
+// 	position: fixed;
+// 	top: 0;
+// 	left: 0;
+// 	right: 0;
+// 	bottom: 0;
+// 	background: rgba(0, 0, 0, 0.5); /* 반투명 배경 */
+// 	display: flex;
+// 	align-items: center;
+// 	justify-content: center;
+// 	z-index: 1000;
+// `;
 
 export const ModalWrapper = styled.div`
-	position: fixed;
-	z-index: 200;
-	border: 0.05rem solid black;
-	background-color: white;
-	padding: 2.5rem;
-	text-align: start;
-	box-sizing: border-box;
+	// position: fixed;
+	// z-index: 200;
+	// border: 0.05rem solid black;
+	// background-color: white;
+	// padding: 2.5rem;
+	// text-align: start;
+	// box-sizing: border-box;
+	// top: 50%;
+	// left: 50%;
+	// width: ${(props) => (props.width ? `${props.width}px` : '786px')};
+	// max-width: 100%;
+	// max-height: 80vh; /* 최대 높이 조정 */
+	// overflow: auto; /* 기본 스크롤바 숨기기 */
+	// transform: translate(-50%, -50%); /* 모달을 중앙에 배치 */
+	// border-radius: 1rem; /* 모서리 둥글게 */
+	// box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 시각적으로 모달을 돋보이게 하는 그림자 추가 */
+	// z-index: 1001; /* Overlay보다 위에 표시되도록 설정 */
+
+	// &.BounceImmerge {
+	// 	animation: ${immergeBounce} 400ms ease-out forwards;
+	// }
+
+	// &.BounceDismiss {
+	// 	animation: ${dismissBounce} 400ms ease-out forwards;
+	// }
+	width: 600px;
+	height: fit-content;
+	border-radius: 15px;
+	background-color: #fff;
+	position: absolute;
 	top: 50%;
 	left: 50%;
-	width: ${(props) => (props.width ? `${props.width}px` : '786px')};
-	max-width: 100%;
-	max-height: 80vh; /* 최대 높이 조정 */
-	overflow: auto; /* 기본 스크롤바 숨기기 */
-	transform: translate(-50%, -50%); /* 모달을 중앙에 배치 */
-	border-radius: 1rem; /* 모서리 둥글게 */
-	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 시각적으로 모달을 돋보이게 하는 그림자 추가 */
-	z-index: 1001; /* Overlay보다 위에 표시되도록 설정 */
-
-	&.BounceImmerge {
-		animation: ${immergeBounce} 400ms ease-out forwards;
-	}
-
-	&.BounceDismiss {
-		animation: ${dismissBounce} 400ms ease-out forwards;
-	}
+	transform: translate(-50%, -50%);
 `;
 
 export const Title = styled.h1`
@@ -103,8 +111,9 @@ export const Table = styled.table`
 export const ButtonContainer = styled.div`
 	display: flex;
 	justify-content: flex-end;
-	padding-right: 1rem;
+	padding-right: 2rem;
 	margin-top: 1rem;
+	margin-bottom: 0rem;
 `;
 
 export const Button = styled.button`
@@ -123,3 +132,53 @@ export const Button = styled.button`
 		background-color: #059669; /* darker green on hover */
 	}
 `;
+//------------------
+export const Overlay = styled.div`
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background: rgba(0, 0, 0, 0.2);
+	z-index: 9999;
+`;
+
+export const ModalWrap = styled.div`
+	width: 600px;
+	height: fit-content;
+	border-radius: 15px;
+	background-color: #fff;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`;
+
+export const Contents = styled.div`
+	margin: 50px 30px;
+	h1 {
+		font-size: 30px;
+		font-weight: 600;
+		margin-bottom: 60px;
+	}
+	img {
+		margin-top: 60px;
+		width: 300px;
+	}
+`;
+// const Button = styled.button`
+// 	font-size: 14px;
+// 	padding: 10px 20px;
+// 	border: none;
+// 	background-color: #ababab;
+// 	border-radius: 10px;
+// 	color: white;
+// 	font-style: italic;
+// 	font-weight: 200;
+// 	cursor: pointer;
+// 	&:hover {
+// 		background-color: #898989;
+// 	}
+// `;
