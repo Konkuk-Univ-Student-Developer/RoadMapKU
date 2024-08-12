ad3c5bb3168c217b07b01b470497929e55684014
HA <haandy98@konkuk.ac.kr>
2024-08-12 20:08:59 +0900

    로드맵 버튼과 상세페이지 연결

--- src/components/RoadMap/RoadMapCell.jsx
+++ src/components/RoadMap/RoadMapCell.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { immergeBounce, dismissBounce } from '../../Animation/Animation';
+import CourseDetail from '../CourseDetail/CousreDetail';
 
 const StyledCell = styled.div`
 	height: 2rem;
@@ -16,6 +17,7 @@ const StyledCell = styled.div`
 	justify-content: center;
 	text-align: center;
 	transition: background-color 0.3s ease-out;
+	overflow: hidden;
 
 	&:hover,
 	&:active {
@@ -36,10 +38,56 @@ const StyledCell = styled.div`
 	}
 `;
 
+const ButtonWrapper = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100%;
+`;
+
+const LeftButton = styled.div`
+	width: 90%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+
+	&:hover,
+	&:active {
+		background-color: #a9d1b3;
+	}
+`;
+
+const RightButton = styled.div`
+	width: 10%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	&:hover,
+	&:active {
+		background-color: #a9d1b3;
+	}
+`;
+
 const Cell = ({ cellData, rowIndex, onClick, unclickable }) => {
+	const [isOpen1, setIsOpen1] = useState(false);
+	const onClickButton1 = () => {
+		setIsOpen1(true);
+	};
+
 	return (
-		<StyledCell className={unclickable ? 'unclickable' : ''} onClick={() => onClick(cellData, rowIndex)}>
-			{cellData.courseName}
+		<StyledCell className={unclickable ? 'unclickable' : ''}>
+			<ButtonWrapper>
+				<LeftButton onClick={() => onClick(cellData, rowIndex)}>{cellData.courseName}</LeftButton>
+				<RightButton onClick={onClickButton1}>:</RightButton>
+				{isOpen1 && (
+					<CourseDetail
+						onClose={() => {
+							setIsOpen1(false);
+						}}
+					/>
+				)}
+			</ButtonWrapper>
 		</StyledCell>
 	);
 };
