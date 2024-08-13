08bc2029e7e9fdf3850d5062edfaf611ad22e78f
Rune <sungjong5k@naver.com>
2024-08-13 20:09:48 +0900

    배경화면 이미지 추가

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -1,45 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center; /* Center the content vertically */
-	align-items: center; /* Center the content horizontally */
-	width: 100%;
-	height: 100%; /* Ensure the container takes the full height of the viewport */
-	margin: 0 auto; /* Center the container horizontally */
-	padding: 0; /* Remove default padding */
-`;
-
 const MainSection = styled.div`
-	display: flex;
-	width: 100%; /* Ensure full width */
-	height: 100%; /* Ensure full height */
-	justify-content: center;
-	align-items: center; /* Center the content vertically */
-	background-color: white; /* Assuming --main is a CSS variable */
-`;
+	width: 100vw;
+	height: 100vh;
+	padding-top: 70px;
+	background-image: url('./img/HomePicture.png');
+	background-size: cover;
+	background-position: center;
 
-const ImageWrapper = styled.div`
-	flex: 1;
 	display: flex;
-	justify-content: center;
 	align-items: center;
-	padding-left: 0px;
-	box-sizing: border-box;
+	justify-content: center;
 `;
 
-function HeroSection() {
-	return (
-		<MainSection>
-			<Container>
-				<ImageWrapper>
-					<img style={{ width: '100%' }} src={`/img/KU_Herosection.png`} alt="Hero Section Character" />
-				</ImageWrapper>
-			</Container>
-		</MainSection>
-	);
+function HeroSection({ children }) {
+	return <MainSection>{children}</MainSection>;
 }
 
 export default HeroSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
-import HeroSection from './HeroSection';
+import styled from 'styled-components';
 import Header from '../components/Header';
+import HeroSection from './HeroSection';
+
+const HomeContainer = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 
 function Home() {
 	return (
-		<>
+		<HomeContainer>
 			<Header />
-			<HeroSection />
-		</>
+			<HeroSection>
+				<div>hi</div>
+			</HeroSection>
+		</HomeContainer>
 	);
 }
 
