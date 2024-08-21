cdb86c8b9df1dd4a6110e2c6a13de2d1a60248f8
Rune <sungjong5k@naver.com>
2024-08-21 15:20:24 +0900

    설명 텍스트  중앙 정렬 및 단락 나누기

--- src/components/AboutUsContents/AboutUsContens.jsx
+++ src/components/AboutUsContents/AboutUsContens.jsx
@@ -14,13 +14,15 @@ const MainTitle = styled.div`
 	font-weight: 800;
 `;
 
-const SubTitle = styled.p`
+export const SubTitle = styled.p`
 	font-size: 50px;
 	font-weight: 700;
 	color: #056a3f;
 `;
 
 const Description = styled.div`
+	text-align: center;
+	word-wrap: break-word;
 	font-size: 40px;
 	font-weight: 500;
 `;
@@ -29,15 +31,14 @@ const AboutUsContents = () => {
 	return (
 		<Container>
 			<MainTitle>About Us</MainTitle>
-			<SubTitle>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, porro explicabo, voluptatibus aspernatur
-				totam natus consectetur ullam optio corporis mollitia sint. Dolorem beatae repellendus laboriosam fugiat,
-				quisquam provident illo suscipit!
-			</SubTitle>
+			<SubTitle>학생들의 니즈를 100% 충족시키기 위해, 쿠스디가 뜁니다.</SubTitle>
 			<Description>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, eveniet sequi! Laudantium, dolorum. Alias
-				consectetur quidem, vero error eum doloremque optio. Quas assumenda aut optio ad quam adipisci voluptates
-				magnam?
+				문제 제기부터 기획, 개발 및 사용자 테스트로 인한 서비스 분석까지 학생들이 직접 해보면 어떨까? 라는 궁금증과
+				기대감으로 시작했습니다.
+			</Description>
+			<Description>
+				학교에게는 학생들의 니즈와 인사이트를 전달, 학생들이 학교 생활에 불편함을 해소할 수 있도록 저희가 뛰겠습니다.
+				앞으로도 저희 쿠스디의 행보에 많은 관심 부탁드립니다.
 			</Description>
 		</Container>
 	);
