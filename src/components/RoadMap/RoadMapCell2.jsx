0ffb496232fed40c6e5489e07039193ea4f6418e
YeeunKim <katekim512@konkuk.ac.kr>
2024-11-04 14:24:37 +0900

    [Feat] 과목명 버튼 이클립스 버튼 삭제

--- src/components/RoadMap/RoadMapCell2.jsx
+++ src/components/RoadMap/RoadMapCell2.jsx
@@ -1,23 +1,7 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { OverflowMenuHorizontal } from '@carbon/icons-react';
-
 import { immergeBounce, dismissBounce } from '../../Animation/Animation';
 import CourseDetail from '../CourseDetail/CourseDetail';
-
-const EllipsisButton = styled.button`
-	background: none;
-	border: none;
-	cursor: pointer;
-	padding: 0 3px;
-	display: flex;
-	align-items: center;
-	&:hover {
-		color: gray;
-	}
-	position: relative;
-	font-size: 24px; /* Set icon size */
-`;
 
 const Dropdown = styled.div`
 	position: absolute;
@@ -36,8 +20,8 @@ const ButtonWrapper = styled.div`
 	display: flex;
 	flex-direction: row;
 	position: relative;
-	justify-content: space-between; /* 버튼을 오른쪽 고정 */
-	align-items: center; /* 상하 중앙 정렬 */
+	justify-content: space-between;
+	align-items: center;
 	text-align: center;
 `;
 
@@ -58,6 +42,11 @@ const StyledCell = styled.div`
 	background-color: white;
 	cursor: pointer;
 	user-select: none;
+	position: relative;
+
+	&:hover {
+		background-color: #d9d9d9;
+	}
 
 	&.unclickable {
 		pointer-events: none;
@@ -65,9 +54,7 @@ const StyledCell = styled.div`
 	}
 
 	&.isHighlighted {
-		background-color: #fff9c4; /* Highlight 색상 */
-		//background-color: #fff8e1; /* Highlight 색상2 */
-		//background-color: #ffffe0; /* Highlight 색상3 */
+		background-color: #fff9c4;
 	}
 
 	&.Bounce-enter {
@@ -79,25 +66,14 @@ const StyledCell = styled.div`
 	}
 `;
 
-// const CourseTitle = styled.div`
-// 	padding-left: 0.5rem;
-// 	text-overflow: ellipsis;
-// 	white-space: nowrap;
-// 	overflow: hidden;
-// 	display: flex;
-// 	align-items: center; /* 상하 중앙 정렬 */
-
-// 	&.semesterCell {
-// 		padding-left: 0;
-// 	}
-// `;
 const CourseTitle = styled.div`
+	font-family: 'Pretendard-regular';
 	padding: 0 0.7rem;
 	text-overflow: ellipsis;
 	white-space: nowrap;
 	overflow: hidden;
 	display: inline-block;
-	width: 100%; /* 부모의 너비에 맞춤 */
+	width: 100%;
 
 	&.semesterCell {
 		padding-left: 0;
@@ -118,30 +94,26 @@ const Cell2 = ({
 
 	useEffect(() => {
 		const competencyCodes = cellData.competencyCodes;
-		if (Array.isArray(competencyCodes)) {
-			setIsHighlighted(competencyCodes.includes(highlightedCompetency));
-		} else {
-			setIsHighlighted(false);
-		}
+		setIsHighlighted(Array.isArray(competencyCodes) && competencyCodes.includes(highlightedCompetency));
 	}, [cellData, highlightedCompetency]);
 
 	const onClickDetailButton = () => {
 		setIsDetailOpen(true);
-		onDropdownToggle(); // 드롭다운 닫기
+		onDropdownToggle(); // 드롭박스 닫기
 	};
 
 	const onClickRoadmapButton = () => {
 		onClick(cellData, rowIndex);
-		onDropdownToggle(); // 드롭다운 닫기
+		onDropdownToggle(); // 기존 드롭박스 닫기
 	};
 
 	return (
-		<StyledCell className={`${unclickable ? 'unclickable' : ''} ${isHighlighted ? 'isHighlighted' : ''}`}>
+		<StyledCell
+			className={`${unclickable ? 'unclickable' : ''} ${isHighlighted ? 'isHighlighted' : ''}`}
+			onClick={onDropdownToggle}
+		>
 			<ButtonWrapper>
 				<CourseTitle className={cellData.haksuId === '0' ? 'semesterCell' : ''}>{cellData.courseName}</CourseTitle>
-				<EllipsisButton onClick={onDropdownToggle}>
-					<OverflowMenuHorizontal size={24} />
-				</EllipsisButton>
 				{isDropdownOpen && (
 					<Dropdown>
 						<DropdownItem onClick={onClickDetailButton}>상세 정보</DropdownItem>
