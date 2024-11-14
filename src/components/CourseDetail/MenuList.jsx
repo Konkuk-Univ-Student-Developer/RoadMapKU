729a9f405f1c0a920b9aa92ce2911ad4850a5a7b
YeeunKim <katekim512@konkuk.ac.kr>
2024-11-14 23:58:35 +0900

    [Fix] 상세모달 목차 디자인 수정

--- src/components/CourseDetail/MenuList.jsx
+++ src/components/CourseDetail/MenuList.jsx
@@ -2,19 +2,61 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-	margin: 10px 20px;
-	background-color: #f0f0f0;
-	border-radius: 8px;
-	border: 1px solid #f0f0f0;
+	background-color: transparent;
+	padding: 0rem;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+// 디자인 후보 #2
+// const TableContainer = styled.div`
+// 	width: 90%;
+// 	border: 1px solid #ddd;
+// 	display: flex;
+// 	flex-direction: column;
+// 	align-items: center;
+// 	justify-content: center;
+// 	margin-top: 10px;
+// 	margin-bottom: 10px;
+// `;
+
+// const Title = styled.div`
+// 	width: 98%;
+// 	background-color: #036b3f;
+// 	color: #ffffff;
+// 	font-size: 16px;
+// 	font-weight: bold;
+// 	border-bottom: 1px solid #ccc;
+// 	padding: 8px;
+// 	margin-bottom: 5px;
+// 	text-align: center;
+// 	display: flex;
+// 	justify-content: center;
+// `;
+
+const TableContainer = styled.div`
+	width: 90%;
+	background-color: rgba(3, 107, 63, 0.1);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin-top: 10px;
+	margin-bottom: 10px;
 `;
 
 const Title = styled.div`
+	width: 97%;
 	font-size: 16px;
 	font-weight: bold;
 	border-bottom: 1px solid #ccc;
 	padding: 8px;
 	margin-bottom: 5px;
 	text-align: center;
+	display: flex;
+	justify-content: center;
 `;
 const ListContainer = styled.ul`
 	padding: 0;
@@ -22,24 +64,26 @@ const ListContainer = styled.ul`
 	justify-content: space-between;
 	width: 80%;
 	margin: 20px auto;
+	display: flex; /* 추가 */
 `;
 
 const ListItem = styled.li`
 	font-size: 14px;
-	color: #666;
 	cursor: pointer;
 `;
 
 const MenuList = ({ descriptionHandler, informationHandler, competencyHandler, competitionHandler }) => {
 	return (
 		<Container>
-			<Title>목차</Title>
-			<ListContainer>
-				<ListItem onClick={descriptionHandler}>과목 설명</ListItem>
-				<ListItem onClick={informationHandler}>기본 정보</ListItem>
-				<ListItem onClick={competencyHandler}>전공 역량</ListItem>
-				<ListItem onClick={competitionHandler}>수강 신청 경쟁률</ListItem>
-			</ListContainer>
+			<TableContainer>
+				<Title>목차</Title>
+				<ListContainer>
+					<ListItem onClick={descriptionHandler}>과목 설명</ListItem>
+					<ListItem onClick={informationHandler}>기본 정보</ListItem>
+					<ListItem onClick={competencyHandler}>전공 역량</ListItem>
+					<ListItem onClick={competitionHandler}>수강 신청 경쟁률</ListItem>
+				</ListContainer>
+			</TableContainer>
 		</Container>
 	);
 };
