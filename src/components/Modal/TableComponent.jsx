382e914f0e39636cf7b4df2bd8d0549fc5e3bb44
YeeunKim <katekim512@konkuk.ac.kr>
2024-11-14 23:34:57 +0900

    [Fix] 기본 정보 표 디자인 통일

--- src/components/Modal/TableComponent.jsx
+++ src/components/Modal/TableComponent.jsx
@@ -1,32 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+	background-color: transparent;
+	padding: 0rem;
+	width: 100%;
+`;
+
 const TableContainer = styled.div`
 	display: flex;
 	justify-content: center;
 	margin-top: 20px;
 `;
 
-const StyledTable = styled.table`
-	border-collapse: separate;
-	border-spacing: 0;
-	width: 100%;
-	margin: 0rem 20px;
-	border-radius: 5px;
-	overflow: hidden;
-	border: 0.5px solid gray;
+const Table = styled.table`
+	width: 90%;
+	border-collapse: collapse;
+	margin-bottom: 20px;
+	table-layout: fixed;
 `;
+
+// const Th = styled.th`
+// 	font-size: 14px;
+// 	padding: 10px;
+// 	background-color: #036b3f;
+// 	color: #ffffff;
+// 	text-align: center;
+
+// 	&:nth-child(1) {
+// 		width: 20%;
+// 	}
+// 	&:nth-child(2) {
+// 		width: 30%;
+// 	}
+// 	&:nth-child(3) {
+// 		width: 50%;
+// 	}
+// `;
 
 const Td = styled.td`
-	padding: 8px;
+	font-size: 11.5px;
+	padding: 10px;
+	border: 1px solid #ddd;
 	text-align: center;
-	border: 0.25px solid gray;
-	font-size: 14px;
-	width: 8%;
-	background-color: ${({ isHeader }) => (isHeader ? '#036b3f' : 'white')};
-	color: ${({ isHeader }) => (isHeader ? 'white' : 'black')};
+	width: 10%;
+	&:nth-child(1) {
+		background-color: #036b3f;
+		color: #ffffff;
+	}
 `;
-
 // 행렬 변환 함수 (구분, 내용 추가)
 const transposeDataWithHeaders = (data) => {
 	const headers = ['구분', '내용'];
@@ -41,21 +63,23 @@ const TableComponent = ({ data }) => {
 	const transposedData = transposeDataWithHeaders(data);
 
 	return (
-		<TableContainer>
-			<StyledTable>
-				<tbody>
-					{transposedData.map((row, i) => (
-						<tr key={i}>
-							{row.map((cell, j) => (
-								<Td key={j} isHeader={j === 0}>
-									{cell}
-								</Td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</StyledTable>
-		</TableContainer>
+		<Container>
+			<TableContainer>
+				<Table>
+					<tbody>
+						{transposedData.map((row, i) => (
+							<tr key={i}>
+								{row.map((cell, j) => (
+									<Td key={j} isHeader={j === 0}>
+										{cell}
+									</Td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			</TableContainer>
+		</Container>
 	);
 };
 
