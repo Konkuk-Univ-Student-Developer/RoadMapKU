86f661693a29ba54e7687238827e66476802e919
Rune <sungjong5k@naver.com>
2024-11-27 01:51:15 +0900

    Design: 헤더바 width 메인 컨텐츠 width와 통일

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -10,18 +10,31 @@ const theme = {
 };
 
 const HeaderContainer = styled.nav`
-	min-width: 1200px;
+	height: 70px;
 	display: flex;
 	align-items: center;
-	justify-content: space-between;
+	justify-content: center;
 	width: 100%;
 	position: fixed;
 	top: 0;
 	z-index: 50;
-	padding: 10px 20px;
 	background-color: white;
-	border-bottom: 1px solid #e5e7eb;
-	box-sizing: border-box; /* Ensure padding does not cause overflow */
+`;
+
+const HeaderContent = styled.div`
+	min-width: 600px;
+	width: 85%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	background-color: white;
+	box-sizing: border-box;
+`;
+
+const ContentContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
 `;
 
 const HeaderBrand = styled.a`
@@ -30,10 +43,10 @@ const HeaderBrand = styled.a`
 `;
 
 const HeaderLinks = styled.div`
+	width: 70%;
 	display: flex;
-	align-items: center;
+	align-items: start;
 	gap: 2rem;
-	flex: 1; /* Allows the links section to grow and shrink */
 `;
 
 const HeaderLink = styled.a`
@@ -41,6 +54,7 @@ const HeaderLink = styled.a`
 	text-decoration: none;
 	user-select: none;
 	${({ $active }) => ($active ? theme.active.on : theme.active.off)}
+
 	&:hover {
 		color: black;
 	}
@@ -50,24 +64,7 @@ const HeaderActions = styled.div`
 	display: flex;
 	align-items: center;
 	gap: 1rem;
-	overflow: hidden; /* Ensure no overflow issues */
-`;
-
-export const Button = styled.button`
-	background-color: #036b3f; /* main color */
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	color: white;
-	font-size: 0.875rem;
-	font-weight: normal;
-	border-radius: 0.375rem;
-	padding: 0.5rem 1.25rem;
-	transition: background-color 0.2s ease-in-out;
-	cursor: pointer;
-	&:hover {
-		background-color: #059669; /* darker green on hover */
-	}
+	overflow: hidden;
 `;
 
 function HeaderBar() {
@@ -77,35 +74,36 @@ function HeaderBar() {
 	return (
 		<>
 			<HeaderContainer>
-				<HeaderBrand onClick={() => navigate('/')}>
-					<KUMAPLogo />
-				</HeaderBrand>
-				<HeaderLinks>
-					<HeaderLink onClick={() => navigate('/')} $active={pathname === '/'}>
-						홈
-					</HeaderLink>
+				<HeaderContent>
+					<ContentContainer>
+						<HeaderBrand onClick={() => navigate('/')}>
+							<KUMAPLogo />
+						</HeaderBrand>
+						<HeaderLinks>
+							<HeaderLink onClick={() => navigate('/manual')} $active={pathname === '/manual'}>
+								소개
+							</HeaderLink>
 
-					<HeaderLink onClick={() => navigate('/manual')} $active={pathname === '/manual'}>
-						소개
-					</HeaderLink>
+							<HeaderLink onClick={() => navigate('/howtopage')} $active={pathname === '/howtopage'}>
+								사용법
+							</HeaderLink>
 
-					<HeaderLink onClick={() => navigate('/howtopage')} $active={pathname === '/howtopage'}>
-						사용법
-					</HeaderLink>
+							<HeaderLink onClick={() => navigate('/road-map')} $active={pathname === '/road-map'}>
+								KUMAP
+							</HeaderLink>
 
-					<HeaderLink onClick={() => navigate('/road-map')} $active={pathname === '/road-map'}>
-						KUMAP
-					</HeaderLink>
+							<HeaderLink onClick={() => navigate('/about-us')} $active={pathname === '/about-us'}>
+								쿠스디는?
+							</HeaderLink>
+						</HeaderLinks>
+					</ContentContainer>
 
-					<HeaderLink onClick={() => navigate('/about-us')} $active={pathname === '/about-us'}>
-						쿠스디는?
-					</HeaderLink>
-				</HeaderLinks>
-				<HeaderActions>
-					<HeaderBrand href="https://www.konkuk.ac.kr/konkuk/index.do">
-						<img src="img/ku-logo.png" alt="KU Logo" style={{ height: '3rem', marginRight: '1rem' }} />
-					</HeaderBrand>
-				</HeaderActions>
+					<HeaderActions>
+						<HeaderBrand href="https://www.konkuk.ac.kr/konkuk/index.do">
+							<img src="img/ku-logo.png" alt="KU Logo" style={{ height: '3rem' }} />
+						</HeaderBrand>
+					</HeaderActions>
+				</HeaderContent>
 			</HeaderContainer>
 		</>
 	);
