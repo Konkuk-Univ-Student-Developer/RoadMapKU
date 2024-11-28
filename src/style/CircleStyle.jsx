32c233c7de9523fba8f1926f89f0960b89121275
Rune <sungjong5k@naver.com>
2024-11-29 02:23:17 +0900

    CircleStyle의 color prop에 $ 추가

--- src/style/CircleStyle.jsx
+++ src/style/CircleStyle.jsx
@@ -18,14 +18,14 @@ const Circle = styled.div`
 	left: ${(props) => props.$left || 'auto'};
 	width: ${(props) => props.$size || '4rem'};
 	height: ${(props) => props.$size || '4rem'};
-	background-color: ${(props) => props.color || '#3498db'};
+	background-color: ${(props) => props.$color || '#3498db'};
 	z-index: ${(props) => props.$zIndex || '0'};
 	border-radius: 50%;
 	animation: ${bounceAnimation} ${(props) => props.$time || '2'}s infinite ease-in-out;
 `;
 
 const CircleStyle = ({ color, size, top, left, time, zIndex }) => {
-	return <Circle color={color} $size={size} $top={top} $left={left} $time={time} $zIndex={zIndex} />;
+	return <Circle $color={color} $size={size} $top={top} $left={left} $time={time} $zIndex={zIndex} />;
 };
 
 export default CircleStyle;
