2e149ee9efde3c8ccbb24c67f82a81ca9e6ffac9
Rune <sungjong5k@naver.com>
2024-12-01 17:31:25 +0900

    Fix: base url

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const useApi = () => {
 	const serverApi = axios.create({
-		baseURL: 'http://203.252.168.41:8080/'
+		baseURL: 'https://kumap.konkuk.ac.kr'
 	});
 
 	return { serverApi };
