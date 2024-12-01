b2aeb7ecca7b14358154c6e1086c99e76bc05fe8
HA <haandy98@konkuk.ac.kr>
2024-12-02 04:03:18 +0900

    로고 크기 수정

--- src/components/LogoFile/Kulogo.jsx
+++ src/components/LogoFile/Kulogo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import url from './../../img/kumap_logo.png';
 
-const KuLogo = () => <img alt="Kumap Logo" src={url} style={{ height: '40px', marginRight: '20px' }} />;
+const KuLogo = () => <img alt="Kumap Logo" src={url} style={{ height: '3rem', marginRight: '20px' }} />;
 
 export default KuLogo;
