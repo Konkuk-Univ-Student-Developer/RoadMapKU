290782b6d48d04da6d548d2ca5a5a1931c972370
HA <haandy98@konkuk.ac.kr>
2024-12-02 20:03:09 +0900

    문구 수정

--- src/components/AboutUsContents/AboutUsContens.jsx
+++ src/components/AboutUsContents/AboutUsContens.jsx
@@ -45,9 +45,9 @@ const AboutUsContents = () => {
 				<MainTitle>About Us</MainTitle>
 				<SubTitle>학생들의 입장에서 문제를 발견하고 해결합니다.</SubTitle>
 				<Description>
-					&apos;문제 제기부터 기획, 개발 및 사용자 테스트로 인한 서비스 분석까지 학생들이 직접 해보면 어떨까?&apos;
+					&apos;문제 제기부터 기획, 개발 및 사용자 테스트로 인한 서비스 분석까지 학생들이 직접 해보면 어떨까?&apos; 라는
 				</Description>
-				<Description>라는 궁금증과 기대감으로 시작했습니다.</Description>
+				<Description>궁금증과 기대감으로 시작했습니다.</Description>
 				<Description></Description>
 				<Description>
 					학교에게는 학생들의 니즈와 인사이트를 전달, 학생들이 학교 생활에 불편함을 해소할 수 있도록 저희가 뛰겠습니다.
