2d690c38542c51738d60066a307a1fbaf25ae962
Rune <sungjong5k@naver.com>
2025-01-10 02:52:40 +0900

    Feat: 공유 버튼 클릭 시 내 로드맵 전체 데이터도 같이 전송

--- src/hooks/useGA.jsx
+++ src/hooks/useGA.jsx
@@ -1,21 +1,23 @@
 import ReactGA from 'react-ga4';
 
 const useGA = () => {
-	const sendClickShareUrl = () => {
+	const sendClickShareUrl = (selectedMyTableContents) => {
 		ReactGA.send({
 			hitType: 'event',
 			eventCategory: 'Button',
 			eventAction: 'share_url',
-			eventLabel: '링크로 공유'
+			eventLabel: '링크로 공유',
+			value: selectedMyTableContents
 		});
 	};
 
-	const sendClickShareScreenshot = () => {
+	const sendClickShareScreenshot = (selectedMyTableContents) => {
 		ReactGA.send({
 			hitType: 'event',
 			eventCategory: 'Button',
 			eventAction: 'share_screenshot',
-			eventLabel: '스크린샷으로 저장'
+			eventLabel: '스크린샷으로 저장',
+			value: selectedMyTableContents
 		});
 	};
 
