f643483cac2f0400102f8bae7cc4de2053b87326
Rune <sungjong5k@naver.com>
2025-01-13 16:57:36 +0900

    Refactor: 에셋 폴더에 폰트 및 data 파일 추가

---
+++ src/assets/font/index.js
@@ -0,0 +1,4 @@
+export { default as PretendardBold } from './Pretendard-Bold.otf';
+export { default as PretendardRegular } from './Pretendard-Regular.otf';
+export { default as PretendardSemiBold } from './Pretendard-SemiBold.otf';
+export { default as FontCSS } from './font.css';
---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export * as Data from './data';
+export * as Font from './font';
+export * as Img from './img';
