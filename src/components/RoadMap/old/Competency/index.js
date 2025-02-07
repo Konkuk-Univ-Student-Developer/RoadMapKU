604362559089eb475bb1eece6be8a8390e5edefa
HA <haandy98@konkuk.ac.kr>
2025-02-07 16:19:12 +0900

    remove: old 폴더 삭제

--- src/components/RoadMap/old/Competency/index.js
+++
@@ -1,2 +0,0 @@
-export { default as CompetencyCell } from './CompetencyCell';
-export { default as CompetencyTable } from './CompetencyTable';
--- src/components/RoadMap/old/TotalRoadMap/index.js
+++
@@ -1,2 +0,0 @@
-export { default as TotalRoadMapCell } from './TotalRoadMapCell';
-export { default as TotalRoadMapModal } from './TotalRoadMapModal';
