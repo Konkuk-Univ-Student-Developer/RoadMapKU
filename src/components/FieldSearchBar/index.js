e7421ed409b452073869a29ceb3ab83fa55482c6
Rune <sungjong5k@naver.com>
2025-03-14 22:06:27 +0900

    Feat: FieldGridContents에 대한 절대경로 추가

--- src/components/FieldSearchBar/index.js
+++ src/components/FieldSearchBar/index.js
@@ -1,6 +1,7 @@
 export * from './DepartmentList';
 export { default as FieldInput } from './FieldInput';
 export { default as FieldListContents } from './FieldListContents';
+export { default as FieldGridContents } from './FieldGridContents';
 export { default as FieldSearchBar } from './FieldSearchBar';
 export * from './SearchBar';
 export * from './SearchLogContent';
