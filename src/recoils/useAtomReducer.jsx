c4f933d112a41c1325d4238fc605e8f57b25feda
Rune <sungjong5k@naver.com>
2025-03-15 02:16:30 +0900

    Refactor: useRecoilTransaction_UNSTABLE  대신 useRecoilCallback 으로 대체

--- src/recoils/useAtomReducer.jsx
+++ src/recoils/useAtomReducer.jsx
@@ -1,33 +1,35 @@
-import { useRecoilTransaction_UNSTABLE } from 'recoil';
+import { useRecoilCallback } from 'recoil';
 import { selectedFieldState, subjectsInFieldState } from './atoms';
 
 function useAtomActionDispatcher() {
-	const dispatch = useRecoilTransaction_UNSTABLE(({ set }) => (action) => {
-		switch (action.type) {
-			case 'clickMiddle':
-				set(selectedFieldState, { middleField: action.field });
-				set(subjectsInFieldState, []);
-				break;
+	const dispatch = useRecoilCallback(
+		({ set }) =>
+			(action) => {
+				switch (action.type) {
+					case 'clickMiddle':
+						set(selectedFieldState, { middleField: action.field });
+						set(subjectsInFieldState, []);
+						break;
+					case 'clickSmall':
+						set(selectedFieldState, (prevState) => ({
+							...prevState,
+							smallField: action.field
+						}));
+						set(subjectsInFieldState, []);
+						break;
+					case 'clickDetail':
+						set(selectedFieldState, (prevState) => ({
+							...prevState,
+							detailField: action.field
+						}));
+						break;
+					default:
+						break;
+				}
+			},
+		[]
+	);
 
-			case 'clickSmall':
-				set(selectedFieldState, (prevState) => ({
-					...prevState,
-					smallField: action.field
-				}));
-				set(subjectsInFieldState, []);
-				break;
-
-			case 'clickDetail':
-				set(selectedFieldState, (prevState) => ({
-					...prevState,
-					detailField: action.field
-				}));
-				break;
-
-			default:
-				break;
-		}
-	});
 	return { dispatch };
 }
 
